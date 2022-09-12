0fc8fe620f163ecc3d7a8cbb1e3f4746155be545
khld-musa <elex22.sust@gmai.com>
2022-09-12 17:37:38 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('app title'),
+        ),
+        body: Center(
+          child: Text('this is my main body!'),
+        ),
+      ),
+    );
+  }
+}
